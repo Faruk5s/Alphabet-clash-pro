2368144464101dcd4d6e2b48ed0c76d1c647cca4
Faruk5s <smomarfaruk8656@gmail.com>
2024-02-13 16:06:19 -0600

    added list

---
+++ js/alpha.js
@@ -0,0 +1,10 @@
+function play() {
+    const homeSection = document.getElementById('home-screen');
+    homeSection.classList.add('hidden');
+
+    // show playground
+    const playgroundSection = document.getElementById('play-ground');
+    playgroundSection.classList.remove('hidden');
+
+    
+}
