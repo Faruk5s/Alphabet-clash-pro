ac025c9a568af1209dddfde53bb0acf71ffafcf5
Faruk5s <smomarfaruk8656@gmail.com>
2024-02-14 01:13:49 -0600

    added function

--- js/alpha.js
+++ js/alpha.js
@@ -6,5 +6,21 @@ function play() {
     const playgroundSection = document.getElementById('play-ground');
     playgroundSection.classList.remove('hidden');
 
+     const randomAlphabet=getARandomAlphabet();
+     const display=document.getElementById('display');
+     display.innerText=randomAlphabet;
+}
+
+function getARandomAlphabet() {
+    // create alphabet Array
+    const alphabetString ='abcdefghijklmnopqrstuvwxyz';
+    const alphabets =alphabetString.split('');
+    console.log(alphabets)
+    // get random index
+    const randomNumber = Math.random()*25;
+    const index =Math.round(randomNumber);
+
+    const alphabet =alphabets[index];
+    return alphabet;
     
 }
---
+++ js/utility.js
@@ -0,0 +1,12 @@
+// function continueGame() {
+//     const alphabet =getARandomAlphabet();
+//     console.log('Random alphabet', alphabet);
+    
+// }
+
+
+// function play() {
+//     hideElementById('home-screen');
+//     showElementById('play-ground');
+//     continueGame()
+// }
